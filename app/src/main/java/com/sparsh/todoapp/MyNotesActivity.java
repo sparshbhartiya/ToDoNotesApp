c544fc1f942ae859352aa9fce2e2d9896b132c76
Sparsh <sporsh21@gmail.com>
2020-08-10 15:32:13 +0530

    Dialog box implemented

--- app/src/main/java/com/sparsh/todoapp/MyNotesActivity.java
+++ app/src/main/java/com/sparsh/todoapp/MyNotesActivity.java
@@ -1,14 +1,56 @@
 package com.sparsh.todoapp;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MyNotesActivity extends AppCompatActivity {
-
+    String fullName;
+    FloatingActionButton fabAddNotes;
+    TextView textViewTitle,textViewDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
+        fabAddNotes = findViewById(R.id.fabAddNotes);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewDescription = findViewById(R.id.textViewDescription);
+        Intent intent = getIntent();
+        fullName = intent.getStringExtra("full_name");
+        getSupportActionBar().setTitle(fullName);
+        fabAddNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setupDialogBox();
+            }
+        });
+    }
+    private void setupDialogBox(){
+        View view = LayoutInflater.from(MyNotesActivity.this).inflate(R.layout.add_notes_dialog_layout,null);
+        final EditText editTextTitle = view.findViewById(R.id.editTextTitle);
+        final EditText editTextDescription = view.findViewById(R.id.editTextDescription);
+        Button buttonSubmit = view.findViewById(R.id.buttonSubmit);
+        //Dialog(pop up is called dialog in android)
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setView(view).setCancelable(false).create();
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewTitle.setText(editTextTitle.getText().toString());
+                textViewDescription.setText(editTextDescription.getText().toString());
+                dialog.hide();
+            }
+        });
+        dialog.show();
     }
 }
